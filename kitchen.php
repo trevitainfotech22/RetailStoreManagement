9edbf649f9616c18b6f52e69c8a21cc6300d471e
shivanipanchal3004 <shivanipanchalsp30@gmail.com>
2022-07-09 11:00:36 +0530

    commit

--- kitchen.php
+++ kitchen.php
@@ -65,7 +65,7 @@ if (!isset($_SESSION['userid'])) {
                                         echo "<td>".$res['name']."</td>";
                                         echo "<td>". $res['username']."</td>";
                                         echo "<td>".$res['password']."</td>";
-                                        
+                                        echo "<td> <a href='editkitchen.php?id={$res['id']}'>Edit</a> | <a href='kitchen.php ? did={$res['id']}]'>Delete</a> </td>";
                                         }?>
                             </tr> <?php $counter++; } ?>
           </tbody>
